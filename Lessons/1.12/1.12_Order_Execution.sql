422814d0de45c1d3a3ce99c15f2e98c80bc2cabc
Jim Parrera <jim.parrera@gmail.com>
2026-07-04 08:21:45 +0400

    Removed unnecesary folder

--- Lessons/1.12/1.12_Order_Execution.sql
+++
@@ -1,3 +0,0 @@
-SELECT
-    cd.name AS company_name,
-    COUNT(jp)
--- Lessons/1.13/1.13_Project1_Intro.sql
+++
@@ -1,7 +0,0 @@
-SELECT DISTINCT
-    job_country,
-    COUNT(job_country)
-FROM
-    job_postings_fact
-GROUP BY job_country
-ORDER BY COUNT(job_country) DESC;
