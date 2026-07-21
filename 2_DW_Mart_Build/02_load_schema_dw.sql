7d29519178f03916b35ecf39aaa82e8224282ad0
Jim Parrera <jim.parrera@gmail.com>
2026-07-21 07:30:29 +0400

    load schemas and create master build script

---
+++ 2_DW_Mart_Build/02_load_schema_dw.sql
@@ -0,0 +1,39 @@
+-- Step 2: DW - Load data from CSV files into tables
+
+
+SELECT '--- Loading company dim ---' AS info;
+
+INSERT INTO company_dim(company_id, company_name)
+SELECT company_id, name
+FROM read_csv('https://storage.googleapis.com/sql_de/company_dim.csv',
+    AUTO_DETECT=TRUE);
+
+SELECT '--- Loading skills dim ---' AS info;
+
+INSERT INTO skills_dim(skill_id, skill_name, skill_type)
+SELECT skill_id, skills, type
+FROM read_csv('https://storage.googleapis.com/sql_de/skills_dim.csv',
+    AUTO_DETECT=TRUE);
+
+SELECT '--- Loading job postings fact ---' AS info;
+
+INSERT INTO job_postings_fact(
+    job_id, company_id, job_title_short, job_title, job_location,
+    job_via, job_schedule_type, job_work_from_home, search_location,
+    job_posted_date, job_no_degree_mention, job_health_insurance,
+    job_country, salary_rate, salary_year_avg, salary_hour_avg
+)
+SELECT
+    job_id, company_id, job_title_short, job_title, job_location,
+    job_via, job_schedule_type, job_work_from_home, search_location,
+    job_posted_date, job_no_degree_mention, job_health_insurance,
+    job_country, salary_rate, salary_year_avg, salary_hour_avg
+FROM read_csv('https://storage.googleapis.com/sql_de/job_postings_fact.csv',
+    AUTO_DETECT=TRUE);
+
+SELECT '--- Loading skills job dim ---' AS info;
+
+INSERT INTO skills_job_dim(skill_id, job_id)
+SELECT skill_id, job_id
+FROM read_csv('https://storage.googleapis.com/sql_de/skills_job_dim.csv',
+    AUTO_DETECT=TRUE);
